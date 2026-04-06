22976a246efc5cbd5ac4621c395dded3c12ad0dd
avivdan <avivdandino1@gmail.com>
2026-04-07 01:04:26 +0300

    Added s3 seperate terraform init

---
+++ infra/bootstrap/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "project-4-terraform-bucket"
+
+  tags = {
+    Name    = "project-4-terraform-bucket"
+    project = "project-4"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
